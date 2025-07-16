163302cf2e2373c207f4b34e2d10e2fbf37d2893
Samadhan Tilkar <samadhantilkar123@gmail.com>
2025-07-16 18:40:32 +0530

    learn useId hook

--- Week 4/3_UseIdHook/src/LearnUseId/LearnUseId.jsx
+++ Week 4/3_UseIdHook/src/LearnUseId/LearnUseId.jsx
@@ -1,5 +1,14 @@
+import { useId } from "react";
+
 function LearnUseId(){
-    
+
+    const inputId=useId();
+
+    return(
+    <>
+        <label htmlFor={inputId}>Name</label>
+        <input type="text" id={inputId}/>
+    </>)
 }
 
 export default LearnUseId;
